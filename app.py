b9ad182d641fee83e2d00976b9a7609c3b396ecb
Hiromichi Takeuchi1 <ht41kureha@gmail.com>
2026-09-16 22:23:22 +0900

    menu表を作成しました

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+menu = {
+    "D01": {"name": "ブレンドコーヒー", "price": 400},
+    "D02": {"name": "アイスコーヒー", "price": 450},
+    "D03": {"name": "カフェオレ", "price": 500},
+    "D04": {"name": "紅茶", "price": 400},
+    "D05": {"name": "オレンジジュース", "price": 350},
+    "F01": {"name": "ナポリタン", "price": 900},
+    "F02": {"name": "ミートソース", "price": 950},
+    "F03": {"name": "カレーライス", "price": 850},
+    "F04": {"name": "ミックスサンド", "price": 750},
+    "F05": {"name": "グラタン", "price": 900},
+    "S01": {"name": "チーズケーキ", "price": 500},
+    "S02": {"name": "ショートケーキ", "price": 550},
+    "S03": {"name": "プリン", "price": 450},
+    "S04": {"name": "バニラアイス", "price": 400},
+    "S05": {"name": "パンケーキ", "price": 650},
+}
+
+
+@app.route("/menu")
+def show_menu():
+    return render_template("menu.html", menu=menu)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
